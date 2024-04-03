2e18aed03845bdb1c013286845ed085bd91596b8
AmimiHamza <amimi1hamza@gmail.com>
2024-04-03 04:24:24 +0000

    feat: add projects

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+from utils import data
 
 app = Flask(__name__)
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -18,5 +18,15 @@ def about():
 def projects():
     return render_template('projects.html')
 
+@app.route('/description')
+def description():
+    name = request.args.get('name')
+    print(name)
+    
+    url='https://github.com/AmimiHamza/'+name
+    description=data[name][0]
+    image=name+data[name][1]
+    print(image)
+    return render_template('description.html',image=image ,url=url,description=description)
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ utils.py
@@ -0,0 +1,6 @@
+data={'gestion-des-stocks':['description de gestion de stock','.png'],
+      'athar':['descriptiond e athar','.jpg'],
+      'chat-plateform':['description de chat-plateform','.avif'],
+      'web-scraping-platform':['description de web scraping platform','.png'],
+        'telegram_bot':['description de telegram_bot','.png'],
+        'syntaxic-tree-to-assembly':['description de syntaxic-tree-to-assembly','.jpg']}
