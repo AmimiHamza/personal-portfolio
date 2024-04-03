597ef3641fca0ae9cf3c4db9748ec86eea210c94
AmimiHamza <amimi1hamza@gmail.com>
2024-04-03 05:48:24 +0000

    fix:add the correct description

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
-data={'gestion-des-stocks':['description de gestion de stock','.png'],
-      'athar':['descriptiond e athar','.jpg'],
-      'chat-plateform':['description de chat-plateform','.avif'],
-      'web-scraping-platform':['description de web scraping platform','.png'],
-        'telegram_bot':['description de telegram_bot','.png'],
-        'syntaxic-tree-to-assembly':['description de syntaxic-tree-to-assembly','.jpg']}
+data={'gestion-des-stocks':["A robust web application for stock management, offering features such as adding, deleting, and modifying products. It allows businesses to efficiently manage their inventory and track stock movements in real-time.",'.png'],
+      'athar':["A mobile application utilizing augmented reality (AR) to provide information to tourists about monuments in Morocco. Users can use their phone's camera to view information cards about the monuments. Additionally, the application allows users to enter the name of a food dish and get photos and information about it.",'.jpg'],
+      'chat-plateform':["A secure chat platform allowing students to create an account and chat with their classmates. Built with Spring Boot, it offers real-time communication to facilitate collaboration and idea exchange.",'.avif'],
+      'web-scraping-platform':["A web platform allowing users to collect data from static and dynamic websites. Users can create an account, access various scraping tools, and receive analysis on the collected data.",'.png'],
+        'telegram_bot':["A bot developed for WhatsApp and Telegram platforms, aimed at assisting students in their studies. Users can request course and quiz files by simply specifying the course name. The bot is deployed under the name '@THE_DSE_BOT' on PythonAnywhere.",'.png'],
+        'syntaxic-tree-to-assembly':["An online platform that transforms the syntax trees of a programming language into assembly code. It provides a user-friendly environment for developers to compile and test their code, simplifying the compilation process.",'.jpg']}
